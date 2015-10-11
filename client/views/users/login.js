9891c06a7a1c62ddcb4662723fcb08f0b3e413de
Jido Park <parkmap@gmail.com>
2015-10-11 13:20:44 +0900

    facebook update

--- client/views/users/login.js
+++ client/views/users/login.js
@@ -16,6 +16,20 @@ Accounts.ui.config({
     }]
 });
 
+// bonus: get some additional profile info from facebook and cache on the user document
+Accounts.onCreateUser(function(options,user) {
+  check(options, Object);
+  check(user, Object);
+
+  options.profile.email = user.services.facebook.email;
+  options.profile.userName = user.services.facebook.id;
+  options.profile.facebookId = user.services.facebook.id;
+
+  user.profile = options.profile;
+
+  return user;
+});
+
 // Template._loginButtonsLoggedInDropdown.events({
 //     'click #login-buttons-edit-profile': function(event) {
 //         // Router.go('profileEdit');
